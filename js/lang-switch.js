0b92cc912832a959539c485af1fd7129d8cb8dc3
saitoyoshi <mitti180@gmail.com>
2023-04-10 19:01:29 +0900

    英語版ページ完成

--- js/lang-switch.js
+++ js/lang-switch.js
@@ -5,10 +5,14 @@ function switchLang() {
   const selectForm = document.getElementById('lauguage');
   const value = selectForm.value;
   if (value === 'chinese') {
-    // console.log('c');
     location.href = '/mobile-site/zh/index.html';
+    // location.href = '/zh/index.html';
   } else if (value === 'korean') {
     location.href = '/mobile-site/ko/index.html';
-    // console.log('korean');
+    // location.href = '/ko/index.html';
+  } else if (value === 'english') {
+    console.log(location.pathname);
+    location.href = '/mobile-site/en/index.html';
+    // location.href = '/en/index.html';
   }
 }
